f594dd0bcbdba8b41806061f78da443a6d5c2d9c
Michael J. Schultz <mjschultz@gmail.com>
2013-07-28 22:33:51 -0500

    Bootstrap kernels main page

--- kernels/templatetags/kernel_tags.py
+++ kernels/templatetags/kernel_tags.py
@@ -10,11 +10,26 @@ register = template.Library()
 def user_kernels(context, viewer):
     """Given the user associated with the `context`, generate the kernels
     that the `viewer` is able to see."""
+    kernels = Kernel.objects.all()
     try:
         profile_user = context['object']
+        kernels = kernels.filter(owner=profile_user)
     except KeyError:
         profile_user = None
-    kernels = Kernel.objects.filter(owner=profile_user)
     if viewer != profile_user:
         kernels = kernels.filter(access_level=Kernel.ACCESS_PUBLIC)
-    return {'kernels': kernels}
+    context.update({'header': 'Available Kernels',
+                    'kernels': kernels})
+    return context
+
+
+@register.inclusion_tag('kernels/includes/kernels_list.html',
+                        takes_context=True)
+def all_kernels(context):
+    """Show all public kernels."""
+    kernels = Kernel.objects.filter(access_level=Kernel.ACCESS_PUBLIC)
+    context = {}
+    context.update({'header': 'Public Kernels',
+                    'kernels': kernels,
+                    'show_owner': True})
+    return context
